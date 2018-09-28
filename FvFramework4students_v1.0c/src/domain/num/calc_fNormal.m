5dae004b367470b2f799ae59b323e742049ee96f
Tom De Weer <tom.deweer@gmail.com>
2018-09-28 14:10:38 +0200

    Second commit

---
+++ FvFramework4students_v1.0c/src/domain/num/calc_fNormal.m
@@ -0,0 +1,5 @@
+function fNormal = calc_fNormal(fNbVLoc,fNbV,vCoord,fArea)
+   % wijst van kleinste cell index naar grootste
+   implementationnotstarted();
+
+end
---
+++ FvFramework4students_v1.0c/src/domain/num/calc_fTangent.m
@@ -0,0 +1,8 @@
+function fTangent = calc_fTangent(nF,fNormal)
+    % via rechterhandregel: t cross n = ez
+	implementationnotstarted();
+    
+end
+
+
+

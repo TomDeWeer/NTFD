d052bb6048b6d0d65f5d4fb538c93090c2c9da6e
Tom De Weer <tom.deweer@gmail.com>
2018-10-15 17:08:52 +0200

    Added Peclet convection test case, does not work yet.

--- FvFramework4students_v1.0c/work/tests/testConvection.m
+++ FvFramework4students_v1.0c/work/tests/testConvection.m
@@ -2,8 +2,14 @@
 % from left to right
 clear all; close all;
 % Create a mesh
-seedI = LineSeed.lineSeedOneWayBias([0 0],[1 0],5,1.00,'o');
-seedJ = LineSeed.lineSeedOneWayBias([0 0],[0 1],10,1.00,'o');
+Nx = 100
+dx = 1/Nx
+L = 1;
+V = 100;
+D = 16; 
+Pe = L*V/D;
+seedI = LineSeed.lineSeedOneWayBias([0 0],[L 0],Nx,1.00,'o');
+seedJ = LineSeed.lineSeedOneWayBias([0 0],[0 1],4,1.00,'o');
 casedef.boundarynames = {'WESTRAND','OOSTRAND','ZUIDRAND','NOORDRAND'};
 mesh  = TwoSeedMesher.genmesh(seedI,seedJ,casedef.boundarynames);
 % Create domain from mesh
@@ -16,13 +22,13 @@ randomdata = rand(T.elsize,T.elcountzone)-0.5;
 set(T,randomdata);                     % Set with random numbers
 
 U = Field(casedef.dom.allFaces,1);     % Velocity [m/s] (vector);
-set(U,[1000*ones(1,U.elcountzone);zeros(1,U.elcountzone)]);
+set(U,[V*ones(1,U.elcountzone);zeros(1,U.elcountzone)]);
 % reset(U,[1;0.2]); 
 % reset(U,[0; 0]);                          % Reset with all zeros
 casedef.U0 = U;
 
 % Define material properties
-casedef.material.k = 16;  % Thermal conductivity [W/(m K)]
+casedef.material.k = D;  % Thermal conductivity [W/(m K)]
 
 
 % Define boundary conditions
@@ -60,21 +66,22 @@ scale = 'lin'; lw = 1;
 fvmplotfield(result.T,scale,lw);
 
 % Verifying accuracy
-% Getting the temperatures at the horizontal line x=0 to x=1, at y=0.55
+% Getting the temperatures at the horizontal line x=0 to x=1, at y=0.125
 line = zeros(10,1);
 linexloc = zeros(10,1);
 for i=1:result.T.dom.nC
     x = result.T.dom.cCoord(1,i);
     y = result.T.dom.cCoord(2,i);
-    if y < 0.56 && y>0.54 && x>0 && x<1
-        ix = round((x+0.05)/0.1);
+    if y < 0.126 && y>0.124 && x>0 && x<1
+        ix = round((x+(dx*0.5))/dx);
         line(ix) = result.T.data(i);
         linexloc(ix) = x;
     end
 end
 figure()
-plot(line, linexloc)
-realTempF = @(x) (1-x);
+plot(linexloc, line)
+hold on
+realTempF = @(x) 1-((exp(Pe*x)-1)/(exp(Pe)-1));
 realLine = zeros(size(line));
 for i=1:length(line)
     realLine(i) = realTempF(linexloc(i));

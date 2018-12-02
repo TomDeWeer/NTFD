cdbf38226f59287e02f5a78494980daa467ad47d
Tom De Weer <tom.deweer@gmail.com>
2018-12-02 15:35:14 +0100

    SIMPLE test file

---
+++ FvFramework4students_v1.0c/work/tests/momentum/coupled/SIMPLE/testSIMPLE.m
@@ -0,0 +1,94 @@
+clear all; close all;
+
+% Create a mesh
+Lx = 1;
+Ly = 1;
+Nx = 5;
+Ny = 5;
+mu = 4;
+dPdx = -10;
+Uxtop = 0;
+p0 = 5;
+rho = 10;
+nu = mu/rho;
+dx = Lx/Nx;
+dy = Ly/Ny;
+seedI = LineSeed.lineSeedOneWayBias([0 0],[Lx 0],Nx,1.00,'o');
+seedJ = LineSeed.lineSeedOneWayBias([0 0],[0 Ly],Ny,1.00,'o');
+casedef.boundarynames = {'WESTRAND','OOSTRAND','ZUIDRAND','NOORDRAND'};
+mesh  = TwoSeedMesher.genmesh(seedI,seedJ,casedef.boundarynames);
+% Create domain from mesh
+casedef.dom = newdomain(mesh,'MyDomain');
+
+% Set up initial fields
+U = Field(casedef.dom.allCells,1);     % Velocity [m/s] (vector);
+set(U,[ones(1,U.elcountzone);ones(1,U.elcountzone)]);
+casedef.U = U; % initial guess
+P = Field(casedef.dom.allCells,0); % Pressure
+P0 = [];
+for i=1:casedef.dom.nC
+    P0 = [P0, 0];
+end
+set(P,P0)
+casedef.P = P;
+% Define material properties
+casedef.material.nu = nu;  % viscosity [dynamic???]
+casedef.material.rho = rho; % density [kg/m^3]
+
+
+% actual solution
+pfunc = @(x,y) p0 + dPdx*x;
+ufunc = @(x,y) (-dPdx/(2*mu))*y*(Ly-y)+Uxtop*y/Ly;
+vfunc = @(x,y) 0;
+
+% Define boundary conditions
+% There are boundary conditions for velocity and for pressure
+% Wall: dirichlet velocity and neumann pressure in n
+% Prescribed inlet: dirichlet velocity and dirichlet pressure
+% Unprescribed outlet: neumann velocity in n and dirichlet pressure
+jBC = 0;
+jBC = jBC+1;
+casedef.BC{jBC}.zoneID = 'WESTRAND';
+% casedef.BC{jBC}.kind   = 'Dirichlet';
+% casedef.BC{jBC}.data.bcval = @(x,y) [y*(1-y), 0];
+casedef.BC{jBC}.velocityKind   = 'Dirichlet';
+casedef.BC{jBC}.data.velocity = @(x,y) [ufunc(x,y) , vfunc(x,y)]; % uniform velocity inlet
+casedef.BC{jBC}.pressureKind   = 'Dirichlet';
+casedef.BC{jBC}.data.pressure = p0; % prescribed pressure
+jBC = jBC+1;
+casedef.BC{jBC}.zoneID = 'OOSTRAND';
+casedef.BC{jBC}.velocityKind   = 'Neumann';
+casedef.BC{jBC}.data.velocity = [0, 0]; % developed velocity outlet, nonchanging
+casedef.BC{jBC}.pressureKind   = 'Dirichlet';
+casedef.BC{jBC}.data.pressure = 0; % prescribed pressure
+jBC = jBC+1;
+casedef.BC{jBC}.zoneID = 'ZUIDRAND';
+casedef.BC{jBC}.velocityKind   = 'Dirichlet';
+casedef.BC{jBC}.data.velocity = [0 , 0]; % no slip condition
+casedef.BC{jBC}.pressureKind   = 'Neumann';
+casedef.BC{jBC}.data.pressure = 0; % no normal pressure derivative
+jBC = jBC+1;
+casedef.BC{jBC}.zoneID = 'NOORDRAND';
+casedef.BC{jBC}.velocityKind   = 'Dirichlet';
+casedef.BC{jBC}.data.velocity = [0 , 0]; % no slip condition
+casedef.BC{jBC}.pressureKind   = 'Neumann';
+casedef.BC{jBC}.data.pressure = 0; % no normal pressure derivative
+
+% Set up iteration parameters
+casedef.iteration.maxniter = 1000;
+casedef.iteration.UTol     = 1.e-10;
+casedef.iteration.dt = 20;
+
+% Call solver
+result = SIMPLEsolver(casedef);
+
+% Plot result
+% ux
+% figure; hold on; axis off; axis equal; colormap(jet(50));
+% scale = 'lin'; lw = 1; title("Ux"); colorbar();
+% fvmplotfield(result.U,scale,lw, 1);
+% figure; hold on; axis off; axis equal; colormap(jet(50));
+% scale = 'lin'; lw = 1; title("Uy"); colorbar();
+% fvmplotfield(result.U,scale,lw, 2);
+
+

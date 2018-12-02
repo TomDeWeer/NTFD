5a249947cfc0c546325439764bcd77a840232a46
Tom De Weer <tom.deweer@gmail.com>
2018-12-02 19:30:53 +0100

    Added mesh convergence test figure

--- FvFramework4students_v1.0c/work/tests/temperature/convection/testConvection.m
+++ FvFramework4students_v1.0c/work/tests/temperature/convection/testConvection.m
@@ -2,12 +2,13 @@
 % from left to right
 clear all; close all;
 % Create a mesh
-Nx = 100;
+Nx = 640;
 dx = 1/Nx;
 L = 1;
 V = 100;
-D = 16; 
+D = 10; 
 Pe = L*V/D;
+fprintf("Peclet number: %.5f \n",Pe);
 seedI = LineSeed.lineSeedOneWayBias([0 0],[L 0],Nx,1.00,'o');
 seedJ = LineSeed.lineSeedOneWayBias([0 0],[0 1],4,1.00,'o');
 casedef.boundarynames = {'WESTRAND','OOSTRAND','ZUIDRAND','NOORDRAND'};
@@ -67,17 +68,18 @@ fvmplotfield(result.T,scale,lw);
 
 % Verifying accuracy
 % Getting the temperatures at the horizontal line x=0 to x=1, at y=0.125
-line = zeros(10,1);
-linexloc = zeros(10,1);
+line = zeros(10,1); % contains temperatures at horizontal line
+linexloc = zeros(10,1); % contains  x-coord at horizontal line
 for i=1:result.T.dom.nC
     x = result.T.dom.cCoord(1,i);
     y = result.T.dom.cCoord(2,i);
-    if y < 0.126 && y>0.124 && x>0 && x<1
+    if y < 0.126 && y>0.124 && x>0 && x<1 % select if its at y=0.125
         ix = round((x+(dx*0.5))/dx);
         line(ix) = result.T.data(i);
         linexloc(ix) = x;
     end
 end
+% calculate real velocity profile
 figure()
 plot(line, linexloc,'b')
 hold on
@@ -87,4 +89,17 @@ for i=1:length(line)
     realLine(i) = realTempF(linexloc(i));
 end
 %plot(realLine, linexloc,'r')
-err = norm(realLine-line)
+avgErr = mean(abs(realLine-line));
+maxErr = max(abs(realLine-line));
+fprintf("Maximum flow profile error: %.10f \n",maxErr)
+
+
+% mijn eigen meetresultaten: maximum fout ifv x-discretisatie
+Nxs = [5, 10, 20, 40, 80, 160, 320, 640];
+MaximumErrors = [1.00, 0.1065212631, 0.0287998809, 0.0074969523, 0.0019131129, 0.0004832518, 0.0001214419, 0.0000304396];
+% fout deelt duidelijk door 4
+figure()
+loglog(Nxs,MaximumErrors, "x")
+xlabel("Nx")
+ylabel("max error")
+
